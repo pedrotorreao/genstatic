b8c890793ae1e2d47f7b47f5f7521be1b4da6789
pedrotorreao <pedrobarrostorreaogaiao@gmail.com>
2025-11-22 01:50:16 -0300

    deploy feature

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from textnode import TextNode, TextType
 from fs_utils import copy_contents
 from page_handler import generate_pages_recursive
 import os
+import sys
 
 # from htmlnode import HTMLNode
 
@@ -9,17 +10,20 @@ PROJECT_ROOT = r"/home/pedrotorreao/Documents/Projects/bootdev/genstatic"
 
 
 def main():
+    base_path = f"/"
+    if len(sys.argv) > 1:
+        base_path = sys.argv[1]
 
     src_path = r"/home/pedrotorreao/Documents/Projects/bootdev/genstatic/static"
-    dst_path = r"/home/pedrotorreao/Documents/Projects/bootdev/genstatic/public"
+    dst_path = r"/home/pedrotorreao/Documents/Projects/bootdev/genstatic/docs"
 
     copy_contents(src_path, dst_path)
 
     src_path = os.path.join(PROJECT_ROOT, "content")
-    dst_path = os.path.join(PROJECT_ROOT, "public")
+    dst_path = os.path.join(PROJECT_ROOT, "docs")
     template_path = os.path.join(PROJECT_ROOT, "template.html")
 
-    generate_pages_recursive(src_path, template_path, dst_path)
+    generate_pages_recursive(src_path, template_path, dst_path, base_path)
 
     # src_path = os.path.join(PROJECT_ROOT, "content/index.md")
     # dst_path = os.path.join(PROJECT_ROOT, "public/index.html")
--- src/page_handler.py
+++ src/page_handler.py
@@ -3,7 +3,7 @@ from markdown_handler import markdown_to_html_node, extract_title
 from node_handler import *
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, base_path):
     print(
         f"Generating page from {from_path} to {dest_path} using template {template_path}..."
     )
@@ -26,6 +26,10 @@ def generate_page(from_path, template_path, dest_path):
     final_html = template_content.replace("{{ Title }}", md_title).replace(
         "{{ Content }}", html_content
     )
+    # replace 'href="/' with 'href="{base_path}' and 'src="/' with 'src="{base_path}' to handle base paths:
+    final_html = final_html.replace('href="/', f'href="{base_path}').replace(
+        'src="/', f'src="{base_path}'
+    )
 
     # write the final HTML content to the destination file (dest_path) and be sure to create any necessary directories:
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
@@ -35,7 +39,7 @@ def generate_page(from_path, template_path, dest_path):
     print(f"Generated page at {dest_path} using template {template_path}")
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, base_path):
     # crawl every entry in the content directory:
     all_entries = os.listdir(dir_path_content)
     # for each markdown file found, generate a new html file using the same template.html;
@@ -47,11 +51,12 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             dest_path = os.path.join(
                 dest_dir_path, relative_path[:-3] + ".html"
             )  # change .md to .html
-            generate_page(full_entry_path, template_path, dest_path)
+            generate_page(full_entry_path, template_path, dest_path, base_path)
         elif os.path.isdir(full_entry_path):
             # recursively generate pages in subdirectories:
             generate_pages_recursive(
                 full_entry_path,
                 template_path,
                 os.path.join(dest_dir_path, entry),
+                base_path,
             )
